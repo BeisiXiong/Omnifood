56a2bc5fde133a8713e9650d0464856cece4f623
Beisi Xiong <bassxiong@gmail.com>
2018-08-07 13:30:52 +0200

    complete

--- Omnifood/resources/js/scripts.js
+++ Omnifood/resources/js/scripts.js
@@ -42,6 +42,68 @@ $(document).ready(function () {
     }
   });
 });
-     
+     /*animation on scroll*/
+    $('.js--wp-1').waypoint(function(){
+      $('.js--wp-1').addClass('animated fadeIn');        
+    },{
+        
+        offset: '50%'
+    });
+    
+     $('.js--wp-2').waypoint(function(){
+      $('.js--wp-2').addClass('animated fadeInUp');        
+    },{
+        
+        offset: '50%'
+    });
+    
+     $('.js--wp-3').waypoint(function(){
+      $('.js--wp-3').addClass('animated fadeIn');        
+    },{
+        
+        offset: '50%'
+    });
+    
+     $('.js--wp-4').waypoint(function(){
+      $('.js--wp-4').addClass('animated pulse');        
+    },{
+        
+        offset: '50%'
+    });
+    
+    
+    /*Mobile nav*/
+    
+    $('.js--nav-icon').click(function(){
+        
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200); 
+        var icon= $('.js--nav-icon i');
+        
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }else{
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    
+    });
+  /*Maps*/
+    var map = new GMaps({
+  div: '.map',
+  lat: 52.5065133, 
+  lng: 14,
+  zoom: 8       
+});
+    map.addMarker({
+  lat: 52.5065133,
+  lng: 13.1445525,
+  title: 'Lima',
+  infoWindow: {
+  content: '<p>Welcom to Berlin</p>'
+}
+});
+    
     
 });
